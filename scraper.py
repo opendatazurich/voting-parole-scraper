ac8a29d5150892ca805aaa70f78884e1b4a88a6e
Stefan Oderbolz <oderbolz@gmail.com>
2019-05-27 08:48:29 +0200

    Scrape upcoming votes too

--- scraper.py
+++ scraper.py
@@ -44,7 +44,7 @@ def find_vote_name(elem, tag_name, ignore):
             return res
 
 
-def parse_parole_page(content, cursor):
+def parse_parole_page(content):
     soup = BeautifulSoup(content, 'html.parser')
     
     tables = soup.find_all('table')
@@ -69,12 +69,12 @@ def parse_parole_page(content, cursor):
                 if party_text.lower() == 'glp':
                     party_text = party_text.lower()
                 
-                print("%s: %s" % (party.text.strip(), parole.text.strip()))
+                print("%s: %s" % (party_text, parole_text))
                 
                 parole = {
-                    'party': party.text.strip(),
+                    'party': party_text,
                     'party_level': 'communal',
-                    'parole': parole.text.strip()
+                    'parole': parole_text
                 }
                 paroles.append(parole)
         
@@ -86,74 +86,78 @@ def parse_parole_page(content, cursor):
         votes.append(vote)
         
     return votes
+
+def parse_dates_page(date_page_url, conn):
+    date_page = requests.get(date_page_url)
+    soup = BeautifulSoup(date_page.content, 'html.parser')
+    vote_links = soup.select('.mainparsys ul.linklist a.var_icon_arrow_right')
     
-
-
-
-
-# city of zurich
-start_url = 'https://www.stadt-zuerich.ch/portal/de/index/politik_u_recht/abstimmungen_u_wahlen.html'
-
-# check paroles of previous dates
-page = requests.get(start_url)
-soup = BeautifulSoup(page.content, 'html.parser')
-prev_dates_link = soup.find_all("a", href=re.compile("[^#]+.*"), string=re.compile(".*Vergangene Termine.*"))
-prev_url = urljoin(start_url, prev_dates_link[0]['href'])
-
-
-
-prev_page = requests.get(prev_url)
-soup = BeautifulSoup(prev_page.content, 'html.parser')
-
-vote_links = soup.select('.mainparsys ul.linklist a')
-
-#for vote_link in vote_links[:2]:
-try:
+    c = conn.cursor()
     for vote_link in vote_links:
         vote_date = vote_link.text.strip()
         print("")
         print("")
         print(vote_date)
-        vote_url = urljoin(prev_url, vote_link['href'])
+        vote_url = urljoin(date_page_url, vote_link['href'])
         vote_page = requests.get(vote_url)
         soup = BeautifulSoup(vote_page.content, 'html.parser')
         city_vote = soup.find_all(string="Gemeindeabstimmung")
-        if city_vote:
-            voting_parole_link = soup.find("a", string=re.compile(".*(p|P)arole.*"))
-            if not voting_parole_link:
-                print("No voting parole link found")
-                continue
-            parole_url = urljoin(vote_url, voting_parole_link['href'])
-            parole_page = requests.get(parole_url)
-            
-            votes = parse_parole_page(parole_page.content, c)
-            for vote in votes:
-                for parole in vote['paroles']:
-                    c.execute(
-                        '''
-                        INSERT INTO data (
-                            vote_name,
-                            vote_level,
-                            vote_date,
-                            party,
-                            party_level,
-                            parole
-                        )
-                        VALUES
-                        (?,?,?,?,?,?)
-                        ''',
-                        [
-                            vote['vote_name'],
-                            vote['vote_level'],
-                            vote_date,
-                            parole['party'],
-                            parole['party_level'],
-                            parole['parole'],
-                        ]
+        if not city_vote:
+            continue
+        voting_parole_link = soup.find("a", string=re.compile(".*(p|P)arole.*"))
+        if not voting_parole_link:
+            print("No voting parole link found")
+            continue
+        parole_url = urljoin(vote_url, voting_parole_link['href'])
+        parole_page = requests.get(parole_url)
+        
+        votes = parse_parole_page(parole_page.content, c)
+        for vote in votes:
+            for parole in vote['paroles']:
+                c.execute(
+                    '''
+                    INSERT INTO data (
+                        vote_name,
+                        vote_level,
+                        vote_date,
+                        party,
+                        party_level,
+                        parole
                     )
-            conn.commit()
+                    VALUES
+                    (?,?,?,?,?,?)
+                    ''',
+                    [
+                        vote['vote_name'],
+                        vote['vote_level'],
+                        vote_date,
+                        parole['party'],
+                        parole['party_level'],
+                        parole['parole'],
+                    ]
+                )
+        conn.commit()
+    
+
+# city of zurich - start url
+start_url = 'https://www.stadt-zuerich.ch/portal/de/index/politik_u_recht/abstimmungen_u_wahlen.html'
+
+# check paroles of previous and next dates
+page = requests.get(start_url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+prev_dates_link = soup.find_all("a", href=re.compile("[^#]+.*"), string=re.compile(".*Vergangene Termine.*"))
+prev_url = urljoin(start_url, prev_dates_link[0]['href'])
+
+next_dates_link = soup.find_all("a", href=re.compile("[^#]+.*"), string=re.compile(".*Nächste Termine.*"))
+next_url = urljoin(start_url, next_dates_link[0]['href'])
+
+try:
+    parse_dates_page(prev_url, conn)
+    parse_dates_page(next_url, conn)
 except Exception as e:
     print("Error: %s" % e)
     print(traceback.format_exc())
+    raise
 finally:
     conn.close()
